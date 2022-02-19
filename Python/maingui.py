0e6a4111285d0fd993ffa748e3c9ca47b895e72b
danciucu <dancm95@gmail.com>
2022-02-19 23:43:06 +0000

    maingui with some future implementations commented

--- Python/maingui.py
+++ Python/maingui.py
@@ -21,6 +21,27 @@ class MARS(ttkthemes.ThemedTk):
 
         self.tabControl.pack(expand = 1, fill = "both")
 
+        ## create two buttons
+        #self.single_arch_button = tkinter.ttk.Button(self, text = "MARS Single Arch", command = self.single_arch)
+        #self.single_arch_button.place(relx = 0.5, rely = 0.5, anchor = tkinter.CENTER)
+
+        #self.multiple_archs_button = tkinter.ttk.Button(self, text = "MARS Multiple Archs")
+        #self.multiple_archs_button.place(relx = 0.5, rely = 0.55, anchor = tkinter.CENTER)
+
+    #def single_arch(self):
+    #    single_arch_window = tkinter.Toplevel(self)
+    #    single_arch_window.title('Arch 1')
+    #    single_arch_window.geometry('600x600')
+    #    single_arch_window.iconphoto(False, tkinter.PhotoImage(file = "logo.png"))
+
+    #    # create tab control
+    #    tabControl = tkinter.ttk.Notebook(single_arch_window)
+
+    #    sa_tab1 = tab1.Tab1(tabControl)
+    #    sa_tab2 = tab2.Tab2(tabControl)
+    #    sa_tab3 = tab3.Tab3(tabControl)
+
+    #    tabControl.pack(expand = 1, fill = "both")
 
 if __name__ == "__main__":
     app = MARS()
